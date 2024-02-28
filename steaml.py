342b8ba89def013e5f2619ad0b3260181f1a99fd
bravo2024 <161508048+bravo2024@users.noreply.github.com>
2024-02-28 23:40:19 +0530

    Update steaml.py

--- steaml.py
+++ steaml.py
@@ -87,6 +87,8 @@ def display_results(df, forecast):
 
 def display_values(df, forecast):
     # Check if the DataFrame contains 'Date' or 'Datetime' column
+
+    st.write(df.columns)
     date_column = 'Date' if 'Date' in df.columns else 'Datetime'
     
     # Create a DataFrame with 'Date' and 'Close' (actual) columns
