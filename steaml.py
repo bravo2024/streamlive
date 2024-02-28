83d6112e0a7b5ef9e778dfaaa15bae68cf1b0f36
bravo2024 <161508048+bravo2024@users.noreply.github.com>
2024-02-28 23:08:10 +0530

    Update steaml.py
    Last Working

--- steaml.py
+++ steaml.py
@@ -6,14 +6,14 @@ import plotly.graph_objects as go
 import yfinance as yf
 
 # Function to load stock data using Yahoo Finance
-def load_data(symbol, timeframe):
+def load_data(symbol, timeframe, periods):
     end_date = datetime.now()
     if timeframe in ['1m', '5m', '15m', '30m', '1h']:  # Intraday timeframes
         start_date = end_date - timedelta(days=1)  # 1 day of data
     else:  # Higher timeframes
         start_date = end_date - timedelta(days=365)  # 1 year of data
     data = yf.download(symbol, start=start_date, end=end_date, interval=timeframe)
-    return data
+    return data[-periods:]
 
 # Function to train Prophet model
 def train_model(df):
@@ -41,16 +41,6 @@ def predict(model, future):
     return forecast
 
 # Function to display results
-#def display_results(df, forecast):
-  #  fig = go.Figure()
-   # fig.add_trace(go.Candlestick(x=df.index,
- #                   open=df['Open'],
-#                    high=df['High'],
- #                   low=df['Low'],
- #                   close=df['Close'], name='Actual'))
- #   fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Predicted'))
- #   fig.update_layout(title='Actual vs. Predicted Closing Prices')
- #   st.plotly_chart(fig)
 def display_results(df, forecast):
     fig = go.Figure()
     fig.add_trace(go.Candlestick(x=df.index,
@@ -66,15 +56,13 @@ def main():
     st.title("Live Stock Analysis")
     symbol = st.text_input("Enter Stock Symbol (e.g., AAPL for Apple Inc.):")
     timeframe = st.selectbox("Select Timeframe", ['1m', '5m', '15m', '30m', '1h', '1d', '1wk', '1mo'])
+    periods = st.slider("Select Number of Periods", min_value=1, max_value=365, value=3)
 
     if symbol:
-        df = load_data(symbol, timeframe)
+        df = load_data(symbol, timeframe, periods)
         if not df.empty:
             model = train_model(df)
-            if timeframe in ['1m', '5m', '15m', '30m', '1h']:
-                future = model.make_future_dataframe(periods=10, freq='T')  # Adjust periods and frequency as needed
-            else:
-                future = model.make_future_dataframe(periods=10, freq='D')  # Adjust periods and frequency as needed
+            future = model.make_future_dataframe(periods=periods, freq='D' if timeframe in ['1d', '1wk', '1mo'] else 'T')
             forecast = predict(model, future)
             fig = display_results(df, forecast)
             st.plotly_chart(fig)
