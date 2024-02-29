4b79cfc680fa4bc4887f0b9abaa60566cba01fee
bravo2024 <161508048+bravo2024@users.noreply.github.com>
2024-02-29 16:07:11 +0530

    Update streamnew.py

--- streamnew.py
+++ streamnew.py
@@ -15,7 +15,7 @@ from pytz import timezone
 # Function to load stock data using Yahoo Finance
 # Function to load stock data using Yahoo Finance
 # Function to load stock data using Yahoo Finance
-def load_data(symbol, timeframe, num_days=30):
+def load_data(symbol, timeframe, periods=30):
     end_date = datetime.now()
     if timeframe in ['1m', '5m', '15m', '30m', '1h']:  # Intraday timeframes
         start_date = end_date - timedelta(days=1)  # 1 day of data
