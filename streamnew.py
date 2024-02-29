9ea17872b5449ecc279d4e99486a32dc85138eae
bravo2024 <161508048+bravo2024@users.noreply.github.com>
2024-02-29 14:32:04 +0530

    Update streamnew.py

--- streamnew.py
+++ streamnew.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # Function to load stock data using Yahoo Finance
 # Function to load stock data using Yahoo Finance
 # Function to load stock data using Yahoo Finance
-def load_data(symbol, timeframe, periods=100):
+def load_data(symbol, timeframe, num_days=100):
     end_date = datetime.now()
     #if timeframe in ['1m', '5m', '15m', '30m', '1h']:  # Intraday timeframes
      #   start_date = end_date - timedelta(days=100)  # 1 day of data
@@ -27,12 +27,14 @@ def load_data(symbol, timeframe, periods=100):
     
     
     #data = yf.download(symbol, start=start_date, end=end_date, interval=timeframe)
-    start_date = end_date - timedelta(days=periods)
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=num_days)
     df = yf.download(symbol, start=start_date, end=end_date, interval=timeframe)
 
     # Reset index for compatibility with Prophet
     df.reset_index(inplace=True)
-    
+
+    return df
     
     
     
